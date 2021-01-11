4f57bdbbafb105b5b242ab6189cd9ecc5ad69b79
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-11 19:22:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
      if !final_array[name][key]
        !final_array[name][key] = []
  end
- final_array[name][key].push(inner_key)
+ final_array[name][key].push(inner_key).to_s
  end
 end
 final_results
