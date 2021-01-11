8b39fa603cf8a2ca4f0dfb9fff23460203a9cd3e
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-11 19:07:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
  value.each do |inner_key, names|
    names.each do |name|
-     binding.pry
      if !final_array[name]
        final_array[name] = {}
      end
